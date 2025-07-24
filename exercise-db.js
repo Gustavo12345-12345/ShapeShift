8770f84ed27535e8e479aa5d9a64a7b7f8b3584a
Gustavo12345-12345 <85851468+Gustavo12345-12345@users.noreply.github.com>
2025-07-24 18:03:04 -0300

    Update exercise-db.js

--- exercise-db.js
+++ exercise-db.js
@@ -4,33 +4,97 @@
 // Fonte dos GIFs: https://github.com/yuhonas/free-exercise-db (um recurso público e gratuito)
 
 export const exerciseDB = {
-    // Peito
+    // ================== PEITO (CHEST) ==================
     supinoreto: "https://www.yuhonas.com/free-exercise-db/images/barbell-bench-press-1.gif",
     supinoinclinado: "https://www.yuhonas.com/free-exercise-db/images/barbell-incline-bench-press-1.gif",
     supinodeclinado: "https://www.yuhonas.com/free-exercise-db/images/barbell-decline-bench-press-1.gif",
+    supinoretocomhalteres: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-bench-press-1.gif",
+    supinoinclinadocomhalteres: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-incline-bench-press-1.gif",
     crucifixo: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-fly-1.gif",
+    crucifixoinclinado: "https://www.yuhonas.com/free-exercise-db/images/incline-dumbbell-fly-1.gif",
     flexao: "https://www.yuhonas.com/free-exercise-db/images/push-up-1.gif",
-    
-    // Costas
-    puxadafrontal: "https://www.yuhonas.com/free-exercise-db/images/lat-pulldown-1.gif",
-    remadacurvada: "https://www.yuhonas.com/free-exercise-db/images/bent-over-row-1.gif",
-    barrafixa: "https://www.yuhonas.com/free-exercise-db/images/pull-up-1.gif",
+    flexaodebraco: "https://www.yuhonas.com/free-exercise-db/images/push-up-1.gif",
+    flexaoinclinada: "https://www.yuhonas.com/free-exercise-db/images/incline-push-up-1.gif",
+    flexaodeclinada: "https://www.yuhonas.com/free-exercise-db/images/decline-push-up-1.gif",
+    crossover: "https://www.yuhonas.com/free-exercise-db/images/cable-crossover-1.gif",
+    peckdeck: "https://www.yuhonas.com/free-exercise-db/images/machine-fly-1.gif",
+    pullovercomhalter: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-pullover-1.gif",
 
-    // Pernas
+    // ================== COSTAS (BACK) ==================
+    puxadafrontal: "https://www.yuhonas.com/free-exercise-db/images/lat-pulldown-1.gif",
+    puxadafrente: "https://www.yuhonas.com/free-exercise-db/images/lat-pulldown-1.gif",
+    puxadafechada: "https://www.yuhonas.com/free-exercise-db/images/close-grip-lat-pulldown-1.gif",
+    remadacurvada: "https://www.yuhonas.com/free-exercise-db/images/bent-over-row-1.gif",
+    remadacavalinho: "https://www.yuhonas.com/free-exercise-db/images/t-bar-row-1.gif",
+    remadasentada: "https://www.yuhonas.com/free-exercise-db/images/seated-cable-row-1.gif",
+    remadaunilateral: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-row-1.gif",
+    serrote: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-row-1.gif",
+    barrafixa: "https://www.yuhonas.com/free-exercise-db/images/pull-up-1.gif",
+    pulldown: "https://www.yuhonas.com/free-exercise-db/images/straight-arm-pulldown-1.gif",
+    hiperextensao: "https://www.yuhonas.com/free-exercise-db/images/hyperextension-1.gif",
+    goodmorning: "https://www.yuhonas.com/free-exercise-db/images/good-morning-1.gif",
+    encolhimentodeombros: "https://www.yuhonas.com/free-exercise-db/images/barbell-shrug-1.gif",
+
+    // ================== PERNAS (LEGS) ==================
     agachamento: "https://www.yuhonas.com/free-exercise-db/images/barbell-squat-1.gif",
+    agachamentolivre: "https://www.yuhonas.com/free-exercise-db/images/barbell-squat-1.gif",
+    agachamentocombarra: "https://www.yuhonas.com/free-exercise-db/images/barbell-squat-1.gif",
+    agachamentofrontal: "https://www.yuhonas.com/free-exercise-db/images/barbell-front-squat-1.gif",
+    agachamentogoblet: "https://www.yuhonas.com/free-exercise-db/images/goblet-squat-1.gif",
+    agachamentohack: "https://www.yuhonas.com/free-exercise-db/images/hack-squat-1.gif",
     legpress: "https://www.yuhonas.com/free-exercise-db/images/leg-press-1.gif",
     extensaodepernas: "https://www.yuhonas.com/free-exercise-db/images/leg-extension-1.gif",
+    cadeiraextensora: "https://www.yuhonas.com/free-exercise-db/images/leg-extension-1.gif",
+    mesaflexora: "https://www.yuhonas.com/free-exercise-db/images/lying-leg-curl-1.gif",
+    flexaodepernas: "https://www.yuhonas.com/free-exercise-db/images/lying-leg-curl-1.gif",
     levantamentoterra: "https://www.yuhonas.com/free-exercise-db/images/barbell-deadlift-1.gif",
+    deadlift: "https://www.yuhonas.com/free-exercise-db/images/barbell-deadlift-1.gif",
+    stiff: "https://www.yuhonas.com/free-exercise-db/images/romanian-deadlift-1.gif",
+    afundo: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-lunges-1.gif",
+    passada: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-lunges-1.gif",
+    agachamentobulgaro: "https://www.yuhonas.com/free-exercise-db/images/bulgarian-split-squat-1.gif",
+    elevacaodepanturrilha: "https://www.yuhonas.com/free-exercise-db/images/calf-raise-1.gif",
+    panturrilha: "https://www.yuhonas.com/free-exercise-db/images/calf-raise-1.gif",
+    elevacaopelvica: "https://www.yuhonas.com/free-exercise-db/images/hip-thrust-1.gif",
+    hipthrust: "https://www.yuhonas.com/free-exercise-db/images/hip-thrust-1.gif",
 
-    // Ombros
+    // ================== OMBROS (SHOULDERS) ==================
     desenvolvimentomilitar: "https://www.yuhonas.com/free-exercise-db/images/barbell-overhead-press-1.gif",
+    desenvolvimentocomhalteres: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-shoulder-press-1.gif",
+    desenvolvimentoarnold: "https://www.yuhonas.com/free-exercise-db/images/arnold-press-1.gif",
     elevacaolateral: "https://www.yuhonas.com/free-exercise-db/images/lateral-raise-1.gif",
+    elevacaofrontal: "https://www.yuhonas.com/free-exercise-db/images/front-raise-1.gif",
+    remadaalta: "https://www.yuhonas.com/free-exercise-db/images/upright-row-1.gif",
+    facepull: "https://www.yuhonas.com/free-exercise-db/images/face-pull-1.gif",
 
-    // Braços
+    // ================== BÍCEPS ==================
     roscadireta: "https://www.yuhonas.com/free-exercise-db/images/barbell-curl-1.gif",
     roscaalternada: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-curl-1.gif",
+    roscaconcentrada: "https://www.yuhonas.com/free-exercise-db/images/concentration-curl-1.gif",
+    roscascott: "https://www.yuhonas.com/free-exercise-db/images/preacher-curl-1.gif",
+    roscamartelo: "https://www.yuhonas.com/free-exercise-db/images/hammer-curl-1.gif",
+    chinup: "https://www.yuhonas.com/free-exercise-db/images/chin-up-1.gif",
+
+    // ================== TRÍCEPS ==================
     tricepstesta: "https://www.yuhonas.com/free-exercise-db/images/skull-crusher-1.gif",
     tricepscorda: "https://www.yuhonas.com/free-exercise-db/images/triceps-pushdown-1.gif",
+    tricepspulley: "https://www.yuhonas.com/free-exercise-db/images/triceps-pushdown-1.gif",
+    tricepsfrances: "https://www.yuhonas.com/free-exercise-db/images/dumbbell-overhead-triceps-extension-1.gif",
+    mergulhonobanco: "https://www.yuhonas.com/free-exercise-db/images/bench-dips-1.gif",
+    supinofechado: "https://www.yuhonas.com/free-exercise-db/images/close-grip-bench-press-1.gif",
+
+    // ================== ABDÔMEN (ABS) ==================
+    abdominal: "https://www.yuhonas.com/free-exercise-db/images/crunch-1.gif",
+    prancha: "https://www.yuhonas.com/free-exercise-db/images/plank-1.gif",
+    elevacaodepernas: "https://www.yuhonas.com/free-exercise-db/images/leg-raise-1.gif",
+    elevacaodepernasnabarra: "https://www.yuhonas.com/free-exercise-db/images/hanging-leg-raise-1.gif",
+    girocontronco: "https://www.yuhonas.com/free-exercise-db/images/russian-twist-1.gif",
+
+    // ================== CARDIO / CORPO INTEIRO ==================
+    burpee: "https://www.yuhonas.com/free-exercise-db/images/burpee-1.gif",
+    polichinelo: "https://www.yuhonas.com/free-exercise-db/images/jumping-jacks-1.gif",
+    alpinista: "https://www.yuhonas.com/free-exercise-db/images/mountain-climber-1.gif",
+    kettlebellswing: "https://www.yuhonas.com/free-exercise-db/images/kettlebell-swing-1.gif"
 };
 
 /**
