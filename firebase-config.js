008f3a8f1442303a2414642decdf549a6d24d0e4
Gustavo12345-12345 <85851468+Gustavo12345-12345@users.noreply.github.com>
2025-07-24 16:00:15 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Importa as funções necessárias dos módulos do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-// A sua configuração do Firebase.
 const firebaseConfig = {
   apiKey: "AIzaSyAEsQXShcDO-IP4C0mLFBevckA6ccoFry4",
   authDomain: "workoutplaner-3930e.firebaseapp.com",
@@ -14,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-W85Z68R4B9"
 };
 
-// Inicializa o Firebase e exporta as instâncias para serem usadas noutros ficheiros
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
