f1e04543272953bb7f1c23c5afd42c0ee8e5e601
Gustavo12345-12345 <85851468+Gustavo12345-12345@users.noreply.github.com>
2025-07-24 21:07:37 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 // Funções de ajuda compartilhadas, como o processador de texto da IA.
-const helpIconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 5.422a.64.64 0 0 1-.642.642.64.64 0 0 1-.642-.642.64.64 0 0 1 .642-.642.64.64 0 0 1 .642.642z"/></svg>`;
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"><path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34-.294.46-.545.46H6.5c-.5 0-.78-.42-.69-1.01l1.417-6.69a.68.68 0 0 1 .69-.58h.04l1.5 6.926c.012.052.09.12.285.12h.03c.195 0 .308-.04.295-.121l-1.4-6.786a.68.68 0 0 1 .68-.58z"/><circle cx="8" cy="13" r="1"/></svg>;
 
 export function processWorkoutTextToHtml(text, options = {}) {
     const { showStartButton = false } = options;
