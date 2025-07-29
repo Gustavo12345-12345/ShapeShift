43f29492eda6e19838c30198c29972569ec34488
Gustavo12345-12345 <gubeleigoli@gmail.com>
2025-07-29 15:19:58 -0300

    Corrigido erro de sintaxe em firebase-config.js e outros arquivos

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
@@ -18,24 +17,3 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
-=======
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAEsQXShcDO-IP4C0mLFBevckA6ccoFry4",
-  authDomain: "workoutplaner-3930e.firebaseapp.com",
-  projectId: "workoutplaner-3930e",
-  storageBucket: "workoutplaner-3930e.appspot.com",
-  messagingSenderId: "197140468893",
-  appId: "1:197140468893:web:1e43c4072bddb0a4ec3074",
-  measurementId: "G-W85Z68R4B9"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
->>>>>>> 97e4f733a0f2380d536ac6a92a2d41bd0a24e640
