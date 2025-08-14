9ddf99faec53b696523fe5056fabeeef8c74340d
Gustavo12345-12345 <85851468+Gustavo12345-12345@users.noreply.github.com>
2025-08-14 15:31:19 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,27 @@
+/**
+ * firebase-config.js
+ * Este ficheiro inicializa a conexão com o Firebase.
+ *
+ * IMPORTANTE: Substitua o objeto 'firebaseConfig' pelos dados
+ * do seu próprio projeto no painel do Firebase.
+ */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
+// TODO: Substitua o objeto abaixo pelas credenciais do seu projeto Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAEsQXShcDO-IP4C0mLFBevckA6ccoFry4",
-  authDomain: "workoutplaner-3930e.firebaseapp.com",
-  projectId: "workoutplaner-3930e",
-  storageBucket: "workoutplaner-3930e.appspot.com",
-  messagingSenderId: "197140468893",
-  appId: "1:197140468893:web:1e43c4072bddb0a4ec3074",
-  measurementId: "G-W85Z68R4B9"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SUA_APP_ID"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { auth, db };
+// Exporta as instâncias dos serviços para serem usadas em outros ficheiros
+export const auth = getAuth(app);
+export const db = getFirestore(app);
