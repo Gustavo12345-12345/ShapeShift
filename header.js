156c754f3d07a8e215627865eddd1237a7ce1cc7
Gustavo12345-12345 <85851468+Gustavo12345-12345@users.noreply.github.com>
2025-08-16 14:37:54 -0300

    Update header.js

--- header.js
+++ header.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const headerHTML = `
         <div id="main-navigation-container" class="fixed-header">
             <nav>
-                <a href="/index.html" class="${currentPage === '/' || currentPage.includes('index.html') ? 'text-amber-400' : ''}">Gerador</a>
+                <a href="/index.html" class="${currentPage === '/' || currentPage.includes('/index.html') ? 'text-amber-400' : ''}">Gerador</a>
                 <a href="/minha-rotina.html" class="${currentPage.includes('/minha-rotina.html') ? 'text-amber-400' : ''}">Minha Rotina</a>
                 <a href="/planos.html" class="${currentPage.includes('/planos.html') ? 'text-amber-400' : ''}">Planos</a>
             </nav>
